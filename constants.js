f20868b90016fb8627767700bac5ba00853fbd23
Sergey Filenko <sergey.filenko@apriorit.com>
2019-10-23 11:05:06 +0300

    Refactoring

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 //export const uri = 'https://www.onyxpay.co';
-//export const uri = 'https://preprod.onyxpay.co';
-export const uri = 'http://10.100.5.28:3000';
+export const uri = 'https://preprod.onyxpay.co';
+//export const uri = 'http://10.100.5.28:3000';
 
 export const walletPath = '/storage/emulated/0/Onyxpay';
 
